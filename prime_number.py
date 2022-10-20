7e3fadc5992805617fe7770471841328ce3cb2e1
Alen Antony <alenantony32@gmail.com>
2022-10-20 23:59:55 +0530

    Prime number

---
+++ prime_number.py
@@ -0,0 +1,12 @@
+num = int(input('enter a number: '))
+
+if num > 1:
+    for i in range(2, int(num / 2) + 1):
+        if num % i == 0:
+            print('not prime')
+            break
+
+    else:
+        print('prime')
+else:
+    print('not prime')
